34510db04d2ae2fa020eb999eec35ae09a3d030b
kukab-shakil <49521206+kukab-shakil@users.noreply.github.com>
2019-06-22 11:01:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfun(){
+    document.getElementById("mobilenumber")
+    var a = document.getElementById("mobilenumber").Value;
+    if(a > 2){
+        alert('do');
+    //   return false;
+    }
+ }
